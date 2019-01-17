40fb483b3b771df36d4683c8d90501313ad710d6
Agustin Pane <agustin.pane@gmail.com>
2019-01-17 16:46:29 -0300

    Added controllers

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const path = require('path')
-const app = express()
+const path = require('path');
 
-app.use(bodyParser.urlencoded({extended: false}))
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, 'public')))
+const errorController = require('./controllers/error');
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+const app = express();
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.listen(3000)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
