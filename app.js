7ee84989f7cbead201d213c4a5fc702bd70e0718
Agustin Pane <agustin.pane@gmail.com>
2019-01-17 15:22:50 -0300

    Express integrated

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const routes = require('./routes')
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+const app = express()
 
-server.listen(3000)
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+app.get('/',(req, res, next) => {
+    console.log("In the middleware ")
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000)
