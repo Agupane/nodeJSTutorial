2d1b208af2e6d053b786c2921b52ca9e3a3f4f0a
Agustin Pane <agustin.pane@gmail.com>
2019-01-17 16:03:51 -0300

    Serving static content

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-
+const path = require('path')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.get('/',(req, res, next) => {
-    console.log("In the middleware ")
-    res.send('<h1>Hello from Express!</h1>')
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
